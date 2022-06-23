6e3c0923ca84aa181c49db0e2f3f71c1399d1435
TKOGNISHU <kyuseishu1593@gmail.com>
2022-06-23 17:42:59 +0700

    Test new framework

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.engine(
     '.hbs',
     handlebars.engine({
-                extname: '.hbs',
+        extname: '.hbs',
     }),
 );
 app.set('view engine', '.hbs');
