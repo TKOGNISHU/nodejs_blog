10d9e7a4908e11cca686ad78dca477ce5e526e7b
TKOGNISHU <kyuseishu1593@gmail.com>
2022-06-23 18:26:59 +0700

    Test husky

--- src/index.js
+++ src/index.js
@@ -34,5 +34,5 @@ app.set('views', path.join(__dirname, '/resource/views'));
 route(app);
 
 app.listen(port, () => {
-                    console.log(`Example app listening on port ${port}`);
+                        console.log(`Example app listening on port ${port}`);
 });
